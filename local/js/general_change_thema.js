6385b4004615dcaee9c881f92fcff2433f0b6f42
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-13 10:53:06 +0300

    debug2

--- local/js/general_change_thema.js
+++ local/js/general_change_thema.js
@@ -16,6 +16,7 @@ BX.sfz.General.ChangeThema = {
         //console.log(usertype); 
     },
     hideHandler: function(ActionPanel, param1, param2) {
+        console.log("window")
         console.log(ActionPanel)
         console.log(param1)
         console.log(param2)
@@ -31,7 +32,8 @@ BX.sfz.General.ChangeThema = {
                 }
             );
     },
-    detailHandler: function(editor, data) {
+    detailHandler: function(par1, par2) {
+        console.log("detailed")
         console.log(editor)
         console.log(data)
     }
