626ba5fe27667e8a928b6b2a2ec8e6af45606754
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-13 12:56:48 +0300

    ihgiugify

--- local/js/general_change_thema.js
+++ local/js/general_change_thema.js
@@ -27,7 +27,31 @@ BX.sfz.General.ChangeThema = {
         //console.log(usertype); 
     },
     hideHandler: function() {
-        console.log("window")
+        var bindElement = BX("user-block");
+		BX.addClass(bindElement, "user-block-active");
+		BX.PopupMenu.show("user-menu", bindElement, [
+                {
+                    text : "Моя страница",
+                    className : "menu-popup-no-icon",
+                    href: "/company/personal/user/35/"
+                },			
+                { text : "Настройка уведомлений", className : "menu-popup-no-icon", onclick : "BXIM.openSettings({'onlyPanel':'notify'})"},
+                { text : "Панель управления", className : "menu-popup-no-icon", href : "/bitrix/admin/"},
+                {'text':'Расширения','items':[{'id':'landing_bind','system':true,'text':'Выбрать базу знаний','onclick':'BX.SidePanel.Instance.open(\'/kb/binding/menu/?menuId=top_panel:user_menu\', {allowChangeHistory: false});','sort':'100','sectionCode':'other'},{'id':'landing_create','system':true,'text':'Создать базу знаний','onclick':'BX.SidePanel.Instance.open(\'/kb/binding/menu/?menuId=top_panel:user_menu&create=Y\', {allowChangeHistory: false});','sort':'100','sectionCode':'other'},{'delimiter':true},{'href':'/marketplace/?placement=USER_PROFILE_MENU','text':'Битрикс24.Маркет'}]},
+                { text : "Выйти", className : "menu-popup-no-icon", href : "/auth/?logout=yes&sessid=" + BX.bitrix_sessid() + "&backurl=" + encodeURIComponent(B24.getBackUrl()) }
+			],
+			{
+				offsetTop: -9,
+				offsetLeft: 40,
+				angle: true,
+				events: {
+					onPopupClose : function() {
+						BX.removeClass(this.bindElement, "user-block-active");
+					}
+				}
+        
+            });
+        //console.log("window")
         //console.log(ActionPanel)
         //console.log(param1)
         //console.log(param2)
