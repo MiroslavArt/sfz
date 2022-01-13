2ce672d0e40511e70c9a561b7798ec87f2a4064f
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-13 11:31:46 +0300

    debugnew

--- local/js/general_change_thema.js
+++ local/js/general_change_thema.js
@@ -3,7 +3,7 @@ BX.namespace('sfz.General.ChangeThema');
 BX.sfz.General.ChangeThema = {
     init: function() {
         BX.addCustomEvent('BX.UI.ActionPanel:created', BX.delegate(this.hideHandler, this));
-        BX.addCustomEvent('BX.Crm.EntityEditorSection:onLayout', BX.delegate(this.detailHandler, this));
+        //BX.addCustomEvent('BX.Crm.EntityEditorSection:onLayout', BX.delegate(this.detailHandler, this));
         /*switch(type) {
             case 'detail':
                 BX.addCustomEvent('BX.Crm.EntityEditor:onInit', BX.delegate(this.detailHandler, this));
@@ -15,28 +15,32 @@ BX.sfz.General.ChangeThema = {
         }*/
         //console.log(usertype); 
     },
-    hideHandler: function(ActionPanel, param1, param2) {
-        console.log("window")
-        console.log(ActionPanel)
-        console.log(param1)
-        console.log(param2)
-        console.log("click")
-        var elem = $("#menu-popup-user-menu")
-        //var elemsTotal = elems.length;
-        console.log(elem); 
-        $('span.menu-popup-item-text').each(
-            function(item)
-                {
-                    console.log(item)
-                    console.log(item.text())
-                }
-            );
+    hideHandler: function() {
+        //console.log("window")
+        //console.log(ActionPanel)
+        //console.log(param1)
+        //console.log(param2)
+        //console.log("click")
+        
+        $(function(){
+            //onPageLoad();
+            var elem = $("#menu-popup-user-menu")
+            //var elemsTotal = elems.length;
+            console.log(elem); 
+            $('span.menu-popup-item-text').each(
+                function(item)
+                    {
+                        console.log(item)
+                        console.log(item.text())
+                    }
+                );
+        });
     },
-    detailHandler: function(par1, par2) {
+    /*detailHandler: function(par1, par2) {
         console.log("detailed")
         console.log(par1)
         console.log(par2)
-    }
+    }*/
 }
 
 BX.sfz.General.ChangeThema.init();
