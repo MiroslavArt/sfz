e8d8ec3b22d93e14fb3093f4a929102211937a65
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-14 13:34:10 +0300

    sfdf

--- local/js/type_requests_filtercontract.js
+++ local/js/type_requests_filtercontract.js
@@ -1,11 +1,16 @@
 BX.namespace('sfz.Type.RequestsFilterContract');
 
 BX.sfz.Type.RequestsFilterContract = {
+    clientid: null, 
     init: function() {
         BX.addCustomEvent('CRM.EntityModel.Change', BX.delegate(this.reacttoChange, this));
     },
     reacttoChange: function(event) {
         console.log(event)
+        if(typeof field === 'object') {
+            console.log(event._settings.data.REQUISITES[0].enitityID); 
+        }
+        console.log(BX.Crm.EntityEditorSection)
     }
 }
 
