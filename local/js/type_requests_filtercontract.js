b5d246d1fcc83be182148c3b3fa5132872a6e315
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-14 20:09:56 +0300

    ohuhoi

--- local/js/type_requests_filtercontract.js
+++ local/js/type_requests_filtercontract.js
@@ -52,7 +52,7 @@ BX.sfz.Type.RequestsFilterContract = {
                 //var up_names = document.getElementsByName("UF_CRM_1_1642152336");
                 console.log(up_names)
                 console.log(typeof up_names)
-                console.log(up_names.remove())
+                //console.log(up_names.remove())
                 //console.log(up_names.length())
                 //let myArray = Array.from(up_names)
                 //console.log(myArray)
