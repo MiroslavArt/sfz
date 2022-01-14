35f2c8d5b126c81344f3674328a175b461b5fa69
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-14 14:53:59 +0300

    uhugiogbi

--- local/js/type_requests_filtercontract.js
+++ local/js/type_requests_filtercontract.js
@@ -6,15 +6,22 @@ BX.sfz.Type.RequestsFilterContract = {
         BX.addCustomEvent('CRM.EntityModel.Change', BX.delegate(this.reacttoChange, this));
     },
     reacttoChange: function(event) {
-        console.log(event)
+        //console.log(event)
         if(typeof event === 'object') {
             //console.log(event._settings.data.REQUISITES[0])
             this.clientid = event._settings.data.REQUISITES[0].entityId
             console.log(this.clientid); 
         }
+        //let elements = document.querySelectorAll('select');
+        
+        for (let elem of document.body.children) {
+            if (elem.matches('select')) {
+              console.log(elem)
+            }
+        }
         //console.log(BX.Crm.EntityEditorSection)
-        var options = $("select").find(`[name=UF_CRM_1_1642152336]`)
-        console.log(options)
+        //var options = $("select").find(`[name=UF_CRM_1_1642152336]`)
+        //console.log(options)
     }
 }
 
