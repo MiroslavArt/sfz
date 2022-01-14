29595f431a55deb3b1d6a2edcc1771603102278f
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-14 20:11:24 +0300

    joho

--- local/js/type_requests_filtercontract.js
+++ local/js/type_requests_filtercontract.js
@@ -47,10 +47,10 @@ BX.sfz.Type.RequestsFilterContract = {
                 //}
                 //var $value = $("[name='UF_CRM_1_1642152336']");
                 //console.log($value.length)
-                
-                var up_names = document.querySelectorAll('[name="UF_CRM_1_1642152336"]');
+                var up_names = document.querySelectorAll('[data-cid="UF_CRM_1_1642152336"]');
+                //var up_names = document.querySelectorAll('[name="UF_CRM_1_1642152336"]');
                 //var up_names = document.getElementsByName("UF_CRM_1_1642152336");
-                console.log(up_names)
+                console.log(up_names[0])
                 console.log(typeof up_names)
                 //console.log(up_names.remove())
                 //console.log(up_names.length())
