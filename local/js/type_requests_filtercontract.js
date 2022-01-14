e29c71195597ba7fbab9a9f68e59f55c498497a7
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-14 16:13:34 +0300

    hihiugv

--- local/js/type_requests_filtercontract.js
+++ local/js/type_requests_filtercontract.js
@@ -15,7 +15,8 @@ BX.sfz.Type.RequestsFilterContract = {
                 console.log(this.clientid)
                 var form = event._formElement;
                 console.log(form)
-                var parentform = $(form).parent().parent().parent().parent().parent();
+                var parentform = $(form).parent();
+                parentform = parentform.parent();
                 //var parentform = $(form).parents('div.ui-entity-editor-content-block');
                 //var parentform = BX.findParent(form, {"class" : "ui-entity-editor-content-block"}, {"data-cid" : "CLIENT"});
                 console.log(parentform)
