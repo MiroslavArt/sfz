7d745b82276658e0fd0bdc72cc44b54dbed7331b
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-15 20:18:53 +0300

    ohohih

--- local/modules/sfz.custom/lib/eventhandlers/main.php
+++ local/modules/sfz.custom/lib/eventhandlers/main.php
@@ -12,13 +12,6 @@ class Main
     {
         $eventManager = EventManager::getInstance();
         $eventManager->addEventHandler('main','OnEpilog', ['\sfz\Custom\EventHandlers\Main','onEpilog']);
-
-        //\CJSCore::RegisterExt('type_requests_filtercontract',
-        //    [
-        //        'js' => '/local/js/type_requests_filtercontract.js',
-                //'css' => '/local/js/itrack.custom/crm_detail_editor_ext/style.css'
-        //    ]
-        //);
     }
 
     public static function onEpilog()
@@ -31,59 +24,18 @@ class Main
         if(hidethema=="Y") {
             $asset->addString('<script>BX.ready(function () {BX.sfz.General.ChangeThema.init("'.$USER->IsAdmin().'");});</script>');
         }
-        
+        $type = '147'; 
+        $contractuf = 'UF_CRM_1_1642152336';
         
         $urlTemplates = [
-            /*'lead_detail' => ltrim(Option::get('crm', 'path_to_lead_details', '', SITE_ID), '/'),
-            'deal_detail' => ltrim(Option::get('crm', 'path_to_deal_details', '', SITE_ID), '/'),
-            'contact_detail' => ltrim(Option::get('crm', 'path_to_contact_details', '', SITE_ID), '/'),
-            'company_detail' => ltrim(Option::get('crm', 'path_to_company_details', '', SITE_ID), '/'),
-            'lead_kanban' => ltrim(Option::get('crm', 'path_to_lead_kanban', '', SITE_ID), '/'),
-            'deal_kanban' => ltrim(Option::get('crm', 'path_to_deal_kanban', '', SITE_ID), '/'),
-            'deal_kanban_category' => ltrim(Option::get('crm', 'path_to_deal_kanban', '', SITE_ID), '/').'category/#category_id#/',
-            'contact_list' => ltrim(Option::get('crm', 'path_to_contact_list', '', SITE_ID), '/'),
-            'company_list' => ltrim(Option::get('crm', 'path_to_company_list', '', SITE_ID), '/'),
-            'tasks_list' => ltrim(Option::get('tasks', 'paths_task_user', '', SITE_ID), '/'),*/
-            'type_detail' => 'crm/type/147/details/#type_id#/'
+            'type_detail' => 'crm/type/'.$type.'/details/#type_id#/'
         ];
 
         $page = \CComponentEngine::parseComponentPath('/', $urlTemplates, $arVars);
         if($page=='type_detail') {
             \CJSCore::init(['type_requests_filtercontract']);
-            $asset->addString('<script>BX.ready(function () {BX.sfz.Type.RequestsFilterContract.init();});</script>');
+            $asset->addString('<script>BX.ready(function () {BX.sfz.Type.RequestsFilterContract.init('.$contractuf.');});</script>');
         }
 
-        
-        //$type = '';
-
-
-        /*if($page !== false) {
-            switch($page) {
-                case 'lead_detail':
-                case 'deal_detail':
-                case 'contact_detail':
-                case 'company_detail':
-                    $type = 'detail';
-                    break;
-                case 'lead_kanban':
-                case 'deal_kanban':
-                case 'deal_kanban_category':
-                    $type = 'kanban';
-                    break;
-                case 'contact_list':
-                case 'company_list':
-                    $type = 'list';
-                    break;
-            }
-        }
-        
-        $asset = Asset::getInstance();
-
-        if($page !== false && $page === 'deal_detail') {
-            \CJSCore::init(['jquery', 'itrack_crm_detail_editor_ext']);
-            $asset->addString('<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"></link>');
-            $asset->addString('<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>');
-            $asset->addString('<script>BX.ready(function () {BX.iTrack.Crm.DetailEditorExt.init();});</script>');
-        }*/
     }
 }
