9723ff3ba36da38cdd541ba0643c11552d3727c4
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-15 20:18:27 +0300

    ggufyy

--- local/js/type_requests_filtercontract.js
+++ local/js/type_requests_filtercontract.js
@@ -2,7 +2,9 @@ BX.namespace('sfz.Type.RequestsFilterContract');
 
 BX.sfz.Type.RequestsFilterContract = {
     clientid: null, 
-    init: function() {
+    contractuf: null,
+    init: function(contuf) {
+        this.contractuf = contuf; 
         BX.addCustomEvent('BX.CRM.EntityEditor:onInit', BX.delegate(this.reacttoChange, this));
     },
     reacttoChange: function(event, data) {
@@ -13,18 +15,22 @@ BX.sfz.Type.RequestsFilterContract = {
             if(reg.test(data.id)) {
                 this.clientid = data.entityId;
                 console.log(this.clientid)
-                var form = event._formElement
+                //var form = event._formElement
 
                 //var up_names = document.querySelectorAll('[data-cid="UF_CRM_1_1642152336"]');
                 //console.log(up_names[0]);
                 //var wrap = up_names[0];
 
                 //var select = document.querySelectorAll('[name="UF_CRM_1_1642152336"]');
-                var select = document.querySelector('[name="UF_CRM_1_1642152336"]');
+                var select = document.querySelector('[name="'+this.contractuf+'"]');
                 console.log(select)
                 if(select !== null) {
                     var options = select.querySelectorAll('option');
-                    options.forEach(o => o.remove());
+
+                    options.forEach(function(option, i, arr) {
+                        console.log(option.value)
+                    });
+                   // options.forEach(o => o.remove());
                 }
             }
         }
