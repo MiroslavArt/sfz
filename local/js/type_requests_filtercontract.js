60bb7da843fc217dd9784ad57d164e5cc5d7a840
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-17 13:42:51 +0300

    dsff

--- local/js/type_requests_filtercontract.js
+++ local/js/type_requests_filtercontract.js
@@ -13,7 +13,7 @@ BX.sfz.Type.RequestsFilterContract = {
         console.log(this.contractuf)
         console.log(this.clientid)
         BX.addCustomEvent('BX.CRM.EntityEditor:onInit', BX.delegate(this.reacttoChange, this));
-        BX.addCustomEvent('BX.UI.EntityEditorField:onLayout', BX.delegate(this.fieldLayoutHandler, this));
+        //BX.addCustomEvent('BX.UI.EntityEditorField:onLayout', BX.delegate(this.fieldLayoutHandler, this));
         if(this.clientid != null) {
             this.requestContracts().then(function(response) {
 
