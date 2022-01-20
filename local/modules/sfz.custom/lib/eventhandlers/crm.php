7f4321b9858db2fcfcda0f4731e0f0d8b7c3b113
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-20 15:14:50 +0300

    ohoh

--- local/modules/sfz.custom/lib/eventhandlers/crm.php
+++ local/modules/sfz.custom/lib/eventhandlers/crm.php
@@ -140,7 +140,7 @@ class Crm
                     'componentData' => [
                         'template' => '',
                         'params' => [
-                            'DEAL_COUNT' => 20,
+                            //'DEAL_COUNT' => 20,
                             'INTERNAL_FILTER' => [
                                 'COMPANY_ID' => 4263
                             ],
@@ -154,7 +154,7 @@ class Crm
                             'NAME_TEMPLATE' => '#LAST_NAME# #NAME# #SECOND_NAME#',
                             'ENABLE_TOOLBAR' => 1,
                             'PRESERVE_HISTORY' => 1,
-                            'ADD_EVENT_NAME' => 'CrmCreateDealFromCompany'
+                            //'ADD_EVENT_NAME' => 'CrmCreateDealFromCompany'
                         ]
                     ]
                 ]
