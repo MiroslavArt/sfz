663f383b7ed40b3f62db89b560cf72e18485d9e9
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-23 18:27:31 +0300

    sdfsdf

--- local/js/general_change_thema.js
+++ local/js/general_change_thema.js
@@ -50,7 +50,7 @@ BX.sfz.General.ChangeThema = {
         });
     },
     changelogo: function() {
-        var img = $(".logo-image-container:first").children().first()
+        var img = $(".logo-image-container:first")
         img.remove()
         //console.log(img)
         //.attr({'src': '/upload/sfz/sfzlogo.png'}, {'srcset': '/upload/sfz/sfzlogo.png'})
