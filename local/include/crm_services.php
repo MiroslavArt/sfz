82a9cd7f5002a449bf372d30ea4acd7cac158f3b
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-24 13:00:59 +0300

    awf

--- local/include/crm_services.php
+++ local/include/crm_services.php
@@ -49,7 +49,7 @@ $container = new class extends Service\Container {
                                 $userId = Service\Container::getInstance()->getContext()->getUserId();
                                 \Bitrix\Main\Diag\Debug::writeToFile($userId, "dataexp2".date("d.m.Y G.i.s"), "__stzexp.log");
                                 \Bitrix\Main\Diag\Debug::writeToFile($item->getData(), "dataexp2".date("d.m.Y G.i.s"), "__stzexp.log");
-                                $factory = Service\Container::getInstance()->getFactory($entityTypeId);
+                                $factory = Service\Container::getInstance()->getFactory(SUPER_ENTITY_TYPE_ID);
                                 $beforeitem = $factory->getItemBeforeSave();
                                 \Bitrix\Main\Diag\Debug::writeToFile($beforeitem->getData(), "dataexp2".date("d.m.Y G.i.s"), "__stzexp.log");
                                 return $result;
