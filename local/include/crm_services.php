7a06e2cef9a3558b4f2a2cefca35ef9538c31528
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-24 12:16:09 +0300

    debug

--- local/include/crm_services.php
+++ local/include/crm_services.php
@@ -6,30 +6,42 @@ use Bitrix\Crm\Service;
 use Bitrix\Crm\Service\Operation;
 use Bitrix\Main\DI;
 
-class MyFactory extends Service\Factory\Dynamic 
-{
-    public function getUpdateOperation(Item $item, Context $context = null): Operation\Update
+$container = new class extends Service\Container {
+    public function getFactory(int $entityTypeId): ?Factory
     {
-        $operation = parent::getUpdateOperation($item, $context);
-        \Bitrix\Main\Diag\Debug::writeToFile($userId, "ops".date("d.m.Y G.i.s"), "__stzexp.log");
-        return $operation->addAction(
-            Operation::ACTION_BEFORE_SAVE,
-            new class extends Operation\Action {
-                public function process(Item $item): Result
+        // it is the same as
+        // DI\ServiceLocator::getInstance()->addInstance('Crm.Service.Factory.Dynamic.150',$factory);
+        if ($entityTypeId === TYPE2ID)
+        {
+            $type = $this->getTypeByEntityTypeId($entityTypeId);
+            // our new custom factory class
+            $factory = new class($type) extends Factory\Dynamic {
+                public function getUpdateOperation(Item $item, Context $context = null): Operation\Update
                 {
-                    $result = new Result();
-                    $userId = Service\Container::getInstance()->getContext()->getUserId();
-                    
-                    \Bitrix\Main\Diag\Debug::writeToFile($userId, "dataexp".date("d.m.Y G.i.s"), "__stzexp.log");
-                    
-                    return $result;
-                }
-            }
-        );
+                    $operation = parent::getUpdateOperation($item, $context);
+                    \Bitrix\Main\Diag\Debug::writeToFile($userId, "ops".date("d.m.Y G.i.s"), "__stzexp.log");
+                    return $operation->addAction(
+                        Operation::ACTION_BEFORE_SAVE,
+                        new class extends Operation\Action {
+                            public function process(Item $item): Result
+                            {
+                                $result = new Result();
+                                $userId = Service\Container::getInstance()->getContext()->getUserId();
+                                
+                                \Bitrix\Main\Diag\Debug::writeToFile($userId, "dataexp".date("d.m.Y G.i.s"), "__stzexp.log");
+                                
+                                return $result;
+                            }
+                        }
+                    );
+                }    
+            };
+            return $factory;
+        }
+        return parent::getFactory($entityTypeId);
     }
-}
-$type = new \Bitrix\Crm\Model\Dynamic\Type('134'); 
+};
+// here we change the container
+DI\ServiceLocator::getInstance()->addInstance('crm.service.container', $container);
 
-$container = new MyFactory($type);
 
-DI\ServiceLocator::getInstance()->addInstance('crm.service.factory.dynamic', $container);
