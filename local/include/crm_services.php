b90b12bdcde32a7c671b8fb9717b34289676a428
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-24 15:21:06 +0300

    debug

--- local/include/crm_services.php
+++ local/include/crm_services.php
@@ -7,36 +7,17 @@ use Bitrix\Crm\Service\Operation;
 use Bitrix\Main\DI;
 use Bitrix\Crm\Service\Factory;
 
-define('SUPER_ENTITY_TYPE_ID', 134);
-
 $container = new class extends Service\Container {
     public function getFactory(int $entityTypeId): ?Factory
     {
+        \Bitrix\Main\Diag\Debug::writeToFile(TYPE2ID, "dataexp1".date("d.m.Y G.i.s"), "__stzexp.log");
         // it is the same as
         // DI\ServiceLocator::getInstance()->addInstance('Crm.Service.Factory.Dynamic.150',$factory);
-        if ($entityTypeId === SUPER_ENTITY_TYPE_ID)
+        if ($entityTypeId == TYPE2ID)
         {
             $type = $this->getTypeByEntityTypeId($entityTypeId);
             // our new custom factory class
             $factory = new class($type) extends Factory\Dynamic {
-                /*public function getDeleteOperation(Item $item, Context $context = null): Operation\Delete
-                {
-                    $operation = parent::getDeleteOperation($item, $context);
-
-                    return $operation->addAction(
-                        Operation::ACTION_AFTER_SAVE,
-                        new class extends Operation\Action {
-                            public function process(Item $item): Result
-                            {
-                                \Bitrix\Main\Diag\Debug::writeToFile('process', "dataexp".date("d.m.Y G.i.s"), "__stzexp.log");
-                                $userId = Service\Container::getInstance()->getContext()->getUserId();
-                                \Bitrix\Main\Diag\Debug::writeToFile($userId, "dataexp".date("d.m.Y G.i.s"), "__stzexp.log");
-                            
-                                return new Result();
-                            }
-                        }
-                    );
-                }*/
                 public function getUpdateOperation(Item $item, Context $context = null): Operation\Update
                 {
                     $operation = parent::getUpdateOperation($item, $context);
