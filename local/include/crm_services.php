10baa789db6d5aaea1b89d94891494894682543f
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-24 16:08:22 +0300

    fix bug

--- local/include/crm_services.php
+++ local/include/crm_services.php
@@ -13,7 +13,7 @@ $container = new class extends Service\Container {
     {
         // it is the same as
         // DI\ServiceLocator::getInstance()->addInstance('Crm.Service.Factory.Dynamic.150',$factory);
-        if ($entityTypeId == TYPE1ID || $entityTypeId == TYPE2ID )
+        if ($entityTypeId == TYPE1ID)
         {
             $type = $this->getTypeByEntityTypeId($entityTypeId);
             // our new custom factory class
@@ -29,65 +29,30 @@ $container = new class extends Service\Container {
                                 $result = new Result();
                                 $typedata = $item->getData(); 
 
-                                if($entityTypeId == TYPE1ID) {
-                                    if($item->isChangedTitle()) {
-                                        $PROPS['name1'] = $typedata['TITLE']; 
-                                    }
-                                    if($item->isChanged(TYPE1UFENG)) {
-                                        $PROPS['name1eng'] = $typedata[TYPE1UFENG]; 
-                                    }
-                                } else {
-                                    if($item->isChangedTitle()) {
-                                        $PROPS['name2'] = $typedata['TITLE']; 
-                                    }
-                                    if($item->isChanged(TYPE1UFENG)) {
-                                        $PROPS['name2eng'] = $typedata[TYPE2UFENG]; 
-                                    }
-                                    if($item->isChanged(TYPE2UFMANSYPLY)) {
-                                        $user = Utils::getUserbycondition(array('=ID' =>$typedata[TYPE2UFMANSYPLY]));
-                                        if($user) {
-                                            $PROPS['managerplyemail'] = $user['EMAIL'];
-                                        }
-                                    }
-                                    if($item->isChanged(TYPE2UFMANLAM)) {
-                                        $user = Utils::getUserbycondition(array('=ID' =>$typedata[TYPE2UFMANLAM]));
-                                        if($user) {
-                                            $PROPS['managerplyemail'] = $user['EMAIL'];
-                                        }
-                                    }
+                                if($item->isChangedTitle()) {
+                                    $PROPS['name1'] = $typedata['TITLE']; 
                                 }
-                                
+                                if($item->isChanged(TYPE1UFENG)) {
+                                    $PROPS['name1eng'] = $typedata[TYPE1UFENG]; 
+                                }
+
                                 //$userId = Service\Container::getInstance()->getContext()->getUserId();
                                 //\Bitrix\Main\Diag\Debug::writeToFile($item->getData(), "dataexp1".date("d.m.Y G.i.s"), "__stzexp.log");
                                 //\Bitrix\Main\Diag\Debug::writeToFile($item->isChangedTitle(), "tchanged".date("d.m.Y G.i.s"), "__stzexp.log");
-                                //\Bitrix\Main\Diag\Debug::writeToFile($item->isChanged('UF_CRM_3_1642771199'), "ufchanged".date("d.m.Y G.i.s"), "__stzexp.log");
-                                if($PROPS && $entityTypeId == TYPE1ID) {
-                                    foreach($PROPS as $key=>$item) {
-                                        $data = [
-                                            'ACTIVE' => 'Y',
-                                            'NAME' => $key,
-                                            'PROPERTY_VALUES' => [
-                                                'NOVOE_ZNACHENIE'=> $item,
-                                                'SKVOZNAYA_KOMPANIYA_1' => $typedata['ID'],
-                                                'DATA_IZMENENIYA' => ConvertTimeStamp(time(), "FULL") 
-                                            ]
-                                        ];
-                                        $id = Utils::createIBlockElement(makeexportIB, $data, []);
-                                    }
-                                } else {
-                                    foreach($PROPS as $key=>$item) {
-                                        $data = [
-                                            'ACTIVE' => 'Y',
-                                            'NAME' => $key,
-                                            'PROPERTY_VALUES' => [
-                                                'NOVOE_ZNACHENIE'=> $item,
-                                                'SKVOZNAYA_KOMPANIYA_2' => $typedata['ID'],
-                                                'DATA_IZMENENIYA' => ConvertTimeStamp(time(), "FULL") 
-                                            ]
-                                        ];
-                                        $id = Utils::createIBlockElement(makeexportIB, $data, []);
-                                    }
+
+                                foreach($PROPS as $key=>$item) {
+                                    $data = [
+                                        'ACTIVE' => 'Y',
+                                        'NAME' => $key,
+                                        'PROPERTY_VALUES' => [
+                                            'NOVOE_ZNACHENIE'=> $item,
+                                            'SKVOZNAYA_KOMPANIYA_1' => $typedata['ID'],
+                                            'DATA_IZMENENIYA' => ConvertTimeStamp(time(), "FULL") 
+                                        ]
+                                    ];
+                                    $id = Utils::createIBlockElement(makeexportIB, $data, []);
                                 }
+                                
                                 return $result;
                             }
                         }
@@ -95,8 +60,66 @@ $container = new class extends Service\Container {
                 }    
             };
             return $factory;
-        } 
+        } elseif($entityTypeId == TYPE2ID) {
+            $type = $this->getTypeByEntityTypeId($entityTypeId);
+            // our new custom factory class
+            $factory = new class($type) extends Factory\Dynamic {
+                public function getUpdateOperation(Item $item, Context $context = null): Operation\Update
+                {
+                    $operation = parent::getUpdateOperation($item, $context);
+                    return $operation->addAction(
+                        Operation::ACTION_BEFORE_SAVE,
+                        new class extends Operation\Action {
+                            public function process(Item $item): Result
+                            {
+                                $result = new Result();
+                                $typedata = $item->getData(); 
 
+                                if($item->isChangedTitle()) {
+                                    $PROPS['name2'] = $typedata['TITLE']; 
+                                }
+                                if($item->isChanged(TYPE1UFENG)) {
+                                    $PROPS['name2eng'] = $typedata[TYPE2UFENG]; 
+                                }
+                                if($item->isChanged(TYPE2UFMANSYPLY)) {
+                                    $user = Utils::getUserbycondition(array('=ID' =>$typedata[TYPE2UFMANSYPLY]));
+                                    if($user) {
+                                        $PROPS['managerplyemail'] = $user['EMAIL'];
+                                    }
+                                }
+                                if($item->isChanged(TYPE2UFMANLAM)) {
+                                    $user = Utils::getUserbycondition(array('=ID' =>$typedata[TYPE2UFMANLAM]));
+                                    if($user) {
+                                        $PROPS['managerplyemail'] = $user['EMAIL'];
+                                    }
+                                }
+                                
+                                //$userId = Service\Container::getInstance()->getContext()->getUserId();
+                                //\Bitrix\Main\Diag\Debug::writeToFile($item->getData(), "dataexp1".date("d.m.Y G.i.s"), "__stzexp.log");
+                                //\Bitrix\Main\Diag\Debug::writeToFile($item->isChangedTitle(), "tchanged".date("d.m.Y G.i.s"), "__stzexp.log");
+                                //\Bitrix\Main\Diag\Debug::writeToFile($item->isChanged('UF_CRM_3_1642771199'), "ufchanged".date("d.m.Y G.i.s"), "__stzexp.log");
+
+                                foreach($PROPS as $key=>$item) {
+                                    $data = [
+                                        'ACTIVE' => 'Y',
+                                        'NAME' => $key,
+                                        'PROPERTY_VALUES' => [
+                                            'NOVOE_ZNACHENIE'=> $item,
+                                            'SKVOZNAYA_KOMPANIYA_2' => $typedata['ID'],
+                                            'DATA_IZMENENIYA' => ConvertTimeStamp(time(), "FULL") 
+                                        ]
+                                    ];
+                                    $id = Utils::createIBlockElement(makeexportIB, $data, []);
+                                }
+                                
+                                return $result;
+                            }
+                        }
+                    );
+                }    
+            };
+            return $factory;
+        }
       return parent::getFactory($entityTypeId);
     }
 };
