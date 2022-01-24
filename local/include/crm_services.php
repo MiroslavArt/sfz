c4874dd79428db7c1d7ab425a850728fc00436fa
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-01-24 12:26:33 +0300

    debug

--- local/include/crm_services.php
+++ local/include/crm_services.php
@@ -13,7 +13,7 @@ $container = new class extends Service\Container {
         //\Bitrix\Main\Diag\Debug::writeToFile($entityTypeId, "ops".date("d.m.Y G.i.s"), "__stzexp.log");
         // it is the same as
         // DI\ServiceLocator::getInstance()->addInstance('Crm.Service.Factory.Dynamic.150',$factory);
-        if ($entityTypeId === '134')
+        if ($entityTypeId == '134')
         {
             \Bitrix\Main\Diag\Debug::writeToFile('match', "ops".date("d.m.Y G.i.s"), "__stzexp.log");
             $type = $this->getTypeByEntityTypeId($entityTypeId);
