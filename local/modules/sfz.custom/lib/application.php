174e67ae19b2aae1adfcfe7a2a1613e4c486bed9
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-02-04 11:06:06 +0300

    delete events

--- local/modules/sfz.custom/lib/application.php
+++ local/modules/sfz.custom/lib/application.php
@@ -82,9 +82,8 @@ class Application
         $eventManager = EventManager::getInstance();
         $eventManager->addEventHandler('crm','onAfterCrmCompanyUpdate', ['\SFZ\Custom\EventHandlers\Crm','onAfterCrmCompanyUpdate']);
         $eventManager->addEventHandler('crm','onBeforeCrmCompanyUpdate', ['\SFZ\Custom\EventHandlers\Crm','onBeforeCrmCompanyUpdate']);
-        $eventManager->addEventHandler('crm','onBeforeRequisiteUpdate', ['\SFZ\Custom\EventHandlers\Crm','onBeforeRequisiteUpdate']);
         $eventManager->addEventHandler('crm','onAfterRequisiteAdd', ['\SFZ\Custom\EventHandlers\Crm','onAfterRequisiteAdd']);
-        $eventManager->addEventHandler('crm','onBeforeRequisiteAdd', ['\SFZ\Custom\EventHandlers\Crm','onBeforeRequisiteAdd']);
+        
         // хандлер таба - пока деактивен
         $eventManager->addEventHandler('crm','onEntityDetailsTabsInitialized', ['\SFZ\Custom\EventHandlers\Crm','onEntityDetailsTabsInitialized']);
         $eventManager->addEventHandler('main','OnProlog', ['\SFZ\Custom\EventHandlers\Main','onProlog']);
