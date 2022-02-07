dc315b9c5af9035b3bd59df564b9b2f44e5ec1de
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-02-07 17:41:25 +0300

    add opp

--- local/modules/sfz.custom/lib/eventhandlers/iblock.php
+++ local/modules/sfz.custom/lib/eventhandlers/iblock.php
@@ -6,24 +6,56 @@ use Bitrix\Main\Config\Option;
 use Bitrix\Main\EventManager;
 use Bitrix\Main\Page\Asset;
 use SFZ\Custom\Helpers\Utils;
+use Bitrix\Crm\Service;
 
 class Iblock
 {
     public static function OnAfterIBlockElementAdd(&$arFields)
     {
-        \Bitrix\Main\Diag\Debug::writeToFile($arFields, "аdd".date("d.m.Y G.i.s"));
-        //global $APPLICATION;
-        //$APPLICATION->throwException("Косяк.");
-        //return false;
+        if($arFields['IBLOCK_ID']==PLYWOODIB) {
+            $manfield = TYPE2UFMANSYPLY; 
+        } elseif($arFields['IBLOCK_ID']==LAMARTYIB) {
+            $manfield = TYPE2UFMANLAM; 
+        }
+        if($manfield) {
+            $element = Utils::getIBlockElementsByConditions($arFields['IBLOCK_ID'], ['ID'=>$arFields['ID']]);
+            $manager = $element['PROPERTIES']['SOTRUDNIK']['VALUE'];
+            $company = $element['PROPERTIES']['SKVOZNAYA_KOMPANIYA_2']['VALUE']; 
+            if($manager && $company) {
+                self::updatethrougcompany($companyid, $manfield, $manager); 
+            }
+        }
     }
 
     public static function OnAfterIBlockElementUpdate(&$arFields)
     {
-        \Bitrix\Main\Diag\Debug::writeToFile($arFields, "аа".date("d.m.Y G.i.s"));
-        //global $APPLICATION;
-        //$APPLICATION->throwException("Косяк.");
-        //return false;
+        if($arFields['IBLOCK_ID']==PLYWOODIB) {
+            $manfield = TYPE2UFMANSYPLY; 
+        } elseif($arFields['IBLOCK_ID']==LAMARTYIB) {
+            $manfield = TYPE2UFMANLAM; 
+        }
+        if($manfield) {
+            $element = Utils::getIBlockElementsByConditions($arFields['IBLOCK_ID'], ['ID'=>$arFields['ID']]);
+
+        }
     }
 
-    
+    private static function updatethrougcompany($companyid, $manfield, $manager) {
+        $factory = Service\Container::getInstance()->getFactory($typeid);
+        $items = $factory->getItems([
+            'select' => [],
+            'filter' => ['ID'=>TYPE2ID]
+        ]);
+        $item = current($items); 
+        if($item) {
+            $item->set($manfield, $manager);
+            $operation = $factory->getUpdateOperation($item);
+            $operation
+                ->disableCheckFields()
+                ->disableBizProc()
+                ->disableCheckAccess()
+            ;
+            $updateResult = $operation->launch();	
+        }
+    }
 }
