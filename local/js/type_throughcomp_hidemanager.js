71d6e994f2d9cc5b9e96b03c57f15cadcba18abf
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-02-08 08:44:24 +0300

    git pull

--- local/js/type_throughcomp_hidemanager.js
+++ local/js/type_throughcomp_hidemanager.js
@@ -1,9 +1,13 @@
 BX.namespace('sfz.Type.HideManagerEdit');
 
 BX.sfz.Type.HideManagerEdit = {
+    ufarr: [],
     init: function(mode, ufarr1, ufarr2) {
+        this.ufarr = [ufarr1, ufarr2]
         if(mode=='hidesection') {
             BX.addCustomEvent('BX.CRM.EntityEditorSection:onLayout', BX.delegate(this.hidesection, this));
+        } else {
+            BX.addCustomEvent('BX.CRM.EntityEditorSection:onLayout', BX.delegate(this.hidesectionchange, this));
         }
     },
     hidesection: function(par1, par2) {
@@ -15,5 +19,16 @@ BX.sfz.Type.HideManagerEdit = {
                 }
             }
         }
-    }
+    }, 
+    hidesectionchange: function(par1, par2) {
+        console.log(par1)
+        if (typeof par2 === 'object') {
+            if (par2.hasOwnProperty('id')) {
+                if(par2.id=='additional') {
+                    const node = par1._wrapper
+                    console.log(node)
+                }
+            }
+        }
+    },
 }
