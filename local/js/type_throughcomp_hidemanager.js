be9f516ea7312319b3139525c0ec604476aab633
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-02-08 08:34:48 +0300

    sect hided

--- local/js/type_throughcomp_hidemanager.js
+++ local/js/type_throughcomp_hidemanager.js
@@ -3,17 +3,14 @@ BX.namespace('sfz.Type.HideManagerEdit');
 BX.sfz.Type.HideManagerEdit = {
     init: function(mode, ufarr1, ufarr2) {
         if(mode=='hidesection') {
-            console.log("switched")
             BX.addCustomEvent('BX.CRM.EntityEditorSection:onLayout', BX.delegate(this.hidesection, this));
         }
     },
     hidesection: function(par1, par2) {
         if (typeof par2 === 'object') {
             if (par2.hasOwnProperty('id')) {
-                console.log("here1")
                 if(par2.id=='additional') {
                     const node = par1._wrapper
-                    console.log(node)
                     $(node).empty();
                 }
             }
