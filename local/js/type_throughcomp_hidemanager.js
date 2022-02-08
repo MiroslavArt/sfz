b1b83035e19411394eb49e7121fcda72508fe50c
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-02-08 08:22:33 +0300

    debug

--- local/js/type_throughcomp_hidemanager.js
+++ local/js/type_throughcomp_hidemanager.js
@@ -6,10 +6,12 @@ BX.sfz.Type.HideManagerEdit = {
         console.log(ufarr1); 
         console.log(ufarr2); 
         if(mode=='hidesection') {
-            BX.addCustomEvent('BX.UI.EntityConfigurationManager:onInitialise', BX.delegate(this.hidesection, this));
+            console.log("switched")
+            BX.addCustomEvent('BX.CRM.EntityEditorSection:onLayout', BX.delegate(this.hidesection, this));
         }
     },
     hidesection: function(par1, par2) {
+       console.log('start'); 
        console.log(par1);
        console.log(par2);
     }
