dc2f65f42df85ac50c7e3b526b92052ea5e6a37a
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-03-15 11:21:04 +0300

    button hide

--- local/js/calendar_hidebooking.js
+++ local/js/calendar_hidebooking.js
@@ -2,6 +2,15 @@ BX.namespace('sfz.Calendar.HideBooking');
 
 BX.sfz.Calendar.HideBooking = {
     init: function(hidegroup) {
-        console.log(hidegroup)
+        if(hidegroup==1) {
+            console.log(hidegroup)
+            this.hidebookingButton();
+        }
+        
+    },
+    hidebookingButton: function() {
+        var button = $('.ui-btn-success').first();
+        console.log(button)
+        button.hide()
     }
 }
