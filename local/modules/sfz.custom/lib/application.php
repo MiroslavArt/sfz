2e846dfec067d2e5f9889c51aaabf864c88bedc6
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-03-19 08:19:50 +0300

    fix

--- local/modules/sfz.custom/lib/application.php
+++ local/modules/sfz.custom/lib/application.php
@@ -96,7 +96,7 @@ class Application
         $eventManager->addEventHandler('iblock','OnAfterIBlockElementUpdate', ['\SFZ\Custom\EventHandlers\Iblock','OnAfterIBlockElementUpdate']);
         // старый хандлер эпилога
         //$eventManager->addEventHandler('main','OnEpilog', ['\SFZ\Custom\EventHandlers\Main','onEpilog']);
-        $eventManager->addEventHandler('socialnetwork','OnFillSocNetFeaturesList', ['\SFZ\Custom\EventHandlers\Socialnetwork',' OnFillSocNetFeaturesList']);
+        $eventManager->addEventHandler('socialnetwork','OnFillSocNetFeaturesList', ['\SFZ\Custom\EventHandlers\Socialnetwork','OnFillSocNetFeaturesList']);
         
     }
 
