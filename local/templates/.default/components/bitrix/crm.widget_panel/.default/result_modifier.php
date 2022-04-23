d9f8ce4be7c6ae214c96fbf6a52f1fc90a2c08d1
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-04-23 15:21:43 +0300

    fix title

--- local/templates/.default/components/bitrix/crm.widget_panel/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/crm.widget_panel/.default/result_modifier.php
@@ -4,12 +4,15 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 \Bitrix\Main\Diag\Debug::writeToFile($arResult, "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
 
-foreach($arResult['ROWS'] as $item) {
-    foreach($item['cells'] as &$secitem) {
+$newarResult = $arResult; 
+
+foreach($newarResult['ROWS'] as $key => $item) {
+    foreach($item['cells'] as $seckey => $secitem) {
         if($secitem['controls'][0]['typeName']=='funnel' && $secitem['controls'][0]['entityTypeName']=='LEAD') {
             \Bitrix\Main\Diag\Debug::writeToFile('inside', "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
-            $secitem['controls'][0]['title'] = 'воронок'; 
+            //$secitem['controls'][0]['title'] = 'воронок'; 
+            $arResult[$key]['cells'][$seckey]['title'] = 'воронок'; 
         }
     }
-    unset($secitem);
+    //unset($secitem);
 }
