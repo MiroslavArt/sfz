08d4839e0b18c40407db900ddcb772f258f59427
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-04-23 16:42:33 +0300

    check filter

--- local/templates/.default/components/bitrix/crm.widget_panel/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/crm.widget_panel/.default/result_modifier.php
@@ -6,35 +6,45 @@ use Bitrix\Main\Loader;
 Loader::includeModule('crm');
 
 $newarResult = $arResult; 
-\Bitrix\Main\Diag\Debug::writeToFile($arResult, "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
+
+$filter = []; 
+
+if($arResult['FILTER_FIELDS']['RESPONSIBLE_ID']) {
+    $filter['ASSIGNED_BY_ID'] = $arResult['FILTER_FIELDS']['RESPONSIBLE_ID'];
+}
+
+if($arResult['FILTER_FIELDS']['PERIOD_from']) {
+    $filter['>=DATE_CREATE'] = $arResult['FILTER_FIELDS']['PERIOD_from'];
+}
+
+if($arResult['FILTER_FIELDS']['PERIOD_to']) {
+    $filter['<=DATE_CREATE'] = $arResult['FILTER_FIELDS']['PERIOD_to'];
+}
 
 foreach($newarResult['ROWS'] as $key => $item) {
     foreach($item['cells'] as $seckey => $secitem) {
         if($secitem['controls'][0]['typeName']=='funnel' && $secitem['controls'][0]['entityTypeName']=='LEAD') {
-            \Bitrix\Main\Diag\Debug::writeToFile($key, "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
-            //$secitem['controls'][0]['title'] = 'воронок'; 
+             
             $currentdata = $arResult['ROWS'][$key]['cells'][$seckey]['data'];
             foreach($currentdata['0']['items'] as &$thirditem) {
-                $opportunitydata = getLeadssum(["STATUS_ID"=>$thirditem['ID']]);
-                //\Bitrix\Main\Diag\Debug::writeToFile($opportunitydata, "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
+                $opportunitydata = getLeadssum(array_merge($filter, ["STATUS_ID"=>$thirditem['ID']]));
+               
 
                 $thirditem['NAME'] = $thirditem['NAME'].' '.$opportunitydata;
             } 
-            //\Bitrix\Main\Diag\Debug::writeToFile($currentdata, "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
+            
 
             $arResult['ROWS'][$key]['cells'][$seckey]['data'] = $currentdata; 
 
             unset($thirditem);
-            //\Bitrix\Main\Diag\Debug::writeToFile($arResult['ROWS'][$key]['cells'][$seckey]['data'], "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
+            
         }
     }
-    //unset($secitem);
+    
 }
 
 function getLeadssum($filter = []) {
     $arFilter = [
-        //"=COMPANY_ID" => 0,
-        //"STATUS_ID" => $stage,
         "CHECK_PERMISSIONS"=>"N" //не проверять права доступа текущего пользователя
     ];
     $arFilter = array_merge($arFilter, $filter);
@@ -48,7 +58,7 @@ function getLeadssum($filter = []) {
     while($lead = $res->Fetch()) {
         $currencies[$lead['ACCOUNT_CURRENCY_ID']] += $lead['OPPORTUNITY_ACCOUNT'];    
     }
-    \Bitrix\Main\Diag\Debug::writeToFile($currencies, "resfunnel".date("d.m.Y G.i.s"), "__debug.log");
+    
     
     $current = http_build_query($currencies, '', ' ');
     return $current;
