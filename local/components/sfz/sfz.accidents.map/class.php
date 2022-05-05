dc04e45759bc17002fee39d9abd788b75efd4d90
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-05-05 09:51:34 +0300

    новый релиз

--- local/components/sfz/sfz.accidents.map/class.php
+++ local/components/sfz/sfz.accidents.map/class.php
@@ -2,18 +2,19 @@
 
 class AccMap extends CBitrixComponent
 {
-    private $arLost;
+    /*private $arLost;
     private $lostId;
     private $companyId;
     private $contractId;
     private $errors;
-    private $userRole;
+    private $userRole;*/
 
     public function onPrepareComponentParams($arParams)
     {
-        $this->companyId = $arParams['CLIENT_ID'];
-        $this->contractId = $arParams['CONTRACT_ID'];
-        $this->lostId = $arParams['LOST_ID'];
+        // параметры для фильтров тут
+        //$this->companyId = $arParams['CLIENT_ID'];
+        //$this->contractId = $arParams['CONTRACT_ID'];
+        //$this->lostId = $arParams['LOST_ID'];
 
         return $arParams;
     }
@@ -24,9 +25,13 @@ class AccMap extends CBitrixComponent
 
         $arResult =& $this->arResult;
 
-        $arResult['testparam'] = 'xxxx';
+        $arResult['testparam'] = 'Здесь будет отображение карты несчастных случаев';
 
         $this->includeComponentTemplate();
+    }
+
+    private function getSize() {
+
     }
 
 
