60cd0407ce295f000adaa10690038aba1c3bf162
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-05-08 08:28:21 +0300

    table

--- local/components/sfz/sfz.accidents.map/templates/.default/template.php
+++ local/components/sfz/sfz.accidents.map/templates/.default/template.php
@@ -5,4 +5,19 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 //print_r($arResult);
 //echo "</pre>";
 ?>
-<div class="sfz-map-wrap"></div>
+<div class="sfz-map-wrap">
+    <table>
+        <tr>
+            <td>Hi, I'm your first cell.</td>
+            <td>I'm your second cell.</td>
+            <td>I'm your third cell.</td>
+            <td>I'm your fourth cell.</td>
+        </tr>
+        <tr>
+            <td>Hi, I'm your first cell.</td>
+            <td>I'm your second cell.</td>
+            <td>I'm your third cell.</td>
+            <td>I'm your fourth cell.</td>
+        </tr>
+    </table>
+</div>
