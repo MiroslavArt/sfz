de37b0afb93f2d46069ceb59d8aaba3cfa8bb50c
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-05-08 08:12:23 +0300

    map

--- local/components/sfz/sfz.accidents.map/templates/.default/template.php
+++ local/components/sfz/sfz.accidents.map/templates/.default/template.php
@@ -6,3 +6,4 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 //echo "</pre>";
 ?>
 <span class="sfz-map-test"><? echo $arResult['testparam']; ?></span>
+<div class="sfz-map-wrap"></div>
