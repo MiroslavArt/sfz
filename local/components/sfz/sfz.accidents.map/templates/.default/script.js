98ba2d2370b802bd74b2747520cbe77cf80fdc96
Miroslav.Lyantsevich <miroslav.lyantsevich@plypan.com>
2022-05-09 17:19:47 +0300

    xxx

--- local/components/sfz/sfz.accidents.map/templates/.default/script.js
+++ local/components/sfz/sfz.accidents.map/templates/.default/script.js
@@ -7,9 +7,19 @@ function Leave(Element) {
 function Appear(Element) {
     //onsole.log(Element)
     var sluchai = $(Element).find("span")
-    console.log(sluchai)
+    var text 
+    $.each(sluchai,function(index,value){
+
+        // действия, которые будут выполняться для каждого элемента массива
+        // index - это текущий индекс элемента массива (число)
+        // value - это значение текущего элемента массива
+        
+        //выведем индекс и значение массива в консоль
+        text += value.text()
+      
+    });
     popup = BX.PopupWindowManager.create(Element.getAttribute("data-id"), null, {
-        content: sluchai,
+        content: text,
         darkMode: true,
         autoHide: true,
         closeIcon: false
